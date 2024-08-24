0baad751a0686f85dfc43e307944afe06b7f322f
shubhamcode2 <shubhamsinghia160@gmail.com>
2024-08-24 23:31:14 +0530

    first commit

--- backend/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://shubhamsinghia160:MonAtlas39@cluster0.nvaystt.mongodb.net/minor-project-food').then(() => console.log("DB connected"));
-}
